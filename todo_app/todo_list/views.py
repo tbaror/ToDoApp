67b5e57fe721eaa898698eb27993ac005e4a5848
Tal Bar-Or <olsysystems@gmail.com>
2019-09-17 21:58:49 +0300

    forms add task

--- todo_app/todo_list/views.py
+++ todo_app/todo_list/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
 from .models import List
+from .forms import ListForm
 
 # Create your views here.
 
 def home(request):
-    all_item = List.objects.all
-    return render(request ,'home.html', {'all_item':all_item})
+
+    if request.method == 'POST':
+        form = ListForm(request.POST or None)
+        if form.is_valid():
+            form.save()
+            all_item = List.objects.all
+            return render(request ,'home.html', {'all_item':all_item})  
+    else:
+
+        all_item = List.objects.all
+        return render(request ,'home.html', {'all_item':all_item})
 
 
 def about(request):
