f224025ef61bdd5ae4f1cde5134927b5a80345f1
Tal Bar-Or <olsysystems@gmail.com>
2019-09-17 17:55:55 +0300

    table wiith db data

--- todo_app/todo_list/views.py
+++ todo_app/todo_list/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from .models import List
 
 # Create your views here.
 
 def home(request):
-    return render(request ,'home.html', {})
+    all_item = List.objects.all
+    return render(request ,'home.html', {'all_item':all_item})
+
+
+def about(request):
+    return render(request ,'about.html', {})    
