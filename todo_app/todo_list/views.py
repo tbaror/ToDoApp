6619a85430bb437defc908aacc2aa32cfd118f31
tbaror <tbaror@gmail.com>
2019-09-18 18:08:11 +0300

    messages and delete db functionality added

--- todo_app/todo_list/views.py
+++ todo_app/todo_list/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import List
 from .forms import ListForm
+from django.contrib import messages
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 
@@ -11,6 +13,7 @@ def home(request):
         if form.is_valid():
             form.save()
             all_item = List.objects.all
+            messages.success(request,('Item Has ben Added To List'))
             return render(request ,'home.html', {'all_item':all_item})  
     else:
 
@@ -19,4 +22,11 @@ def home(request):
 
 
 def about(request):
-    return render(request ,'about.html', {})    
+    return render(request ,'about.html', {})
+
+
+def delete(request, list_id):
+    item = List.objects.get(pk=list_id)
+    item.delete()
+    messages.success(request,('Item Has Been Deleted'))
+    return redirect('home')
