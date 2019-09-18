7ba4e20ea73974e45052645d13adb52f1775d1eb
tbaror <tal_baror@hotmail.com>
2019-09-18 21:01:12 +0300

    final step

--- todo_app/todo_list/views.py
+++ todo_app/todo_list/views.py
@@ -30,3 +30,36 @@ def delete(request, list_id):
     item.delete()
     messages.success(request,('Item Has Been Deleted'))
     return redirect('home')
+
+
+def cross_off(request, list_id):
+    item = List.objects.get(pk=list_id)
+    item.complete = True
+    item.save()
+    return redirect('home')
+
+def uncross(request, list_id):
+    item = List.objects.get(pk=list_id)
+    item.complete = False
+    item.save()
+    return redirect('home')
+
+
+
+def edit(request, list_id):
+    
+    if request.method == 'POST':
+        item = List.objects.get(pk=list_id)
+
+        form = ListForm(request.POST or None, instance=item)
+        if form.is_valid():
+            form.save()
+            
+            messages.success(request,('Item Has been Edited'))
+            return redirect('home')
+    else:
+
+        item = List.objects.get(pk=list_id)
+        return render(request ,'edit.html', {'item': item})
+
+    
